a17e7792a06e8e7f3333f93a048e6c1cbf67f8a1
Daniel Nelson <dnelson@centresource.com>
2013-12-04 12:13:18 -0800

    log to the Rails logger

--- lib/sendgrid.rb
+++ lib/sendgrid.rb
@@ -171,7 +171,7 @@ module SendGrid
           raise ArgumentError.new("Array for #{find} is not the same size as the recipient array") if replace.size != @sg_recipients.size
         end
       end
-      puts "SendGrid X-SMTPAPI: #{sendgrid_json_headers(message)}" if Object.const_defined?("SENDGRID_DEBUG_OUTPUT") && SENDGRID_DEBUG_OUTPUT
+      Rails.logger.info "SendGrid X-SMTPAPI: #{sendgrid_json_headers(message)}" if Object.const_defined?("SENDGRID_DEBUG_OUTPUT") && SENDGRID_DEBUG_OUTPUT
       self.headers['X-SMTPAPI'] = sendgrid_json_headers(message)
       m
     end
@@ -187,7 +187,7 @@ module SendGrid
           raise ArgumentError.new("Array for #{find} is not the same size as the recipient array") if replace.size != @sg_recipients.size
         end
       end
-      puts "SendGrid X-SMTPAPI: #{sendgrid_json_headers(mail)}" if Object.const_defined?("SENDGRID_DEBUG_OUTPUT") && SENDGRID_DEBUG_OUTPUT
+      Rails.logger.info "SendGrid X-SMTPAPI: #{sendgrid_json_headers(mail)}" if Object.const_defined?("SENDGRID_DEBUG_OUTPUT") && SENDGRID_DEBUG_OUTPUT
       @mail['X-SMTPAPI'] = sendgrid_json_headers(mail)
     end
 
